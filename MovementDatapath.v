c101830f247a5aa07ea9164796fe32dfe02ed671
Tim <ninjayoshi@users.noreply.github.com>
2018-03-20 15:37:27 -0400

    Made much more intuitive and so that the crosshair updates at every tick

--- MovementDatapath.v
+++ MovementDatapath.v
@@ -1,46 +1,52 @@
 module MovementDatapath(clk, reset_n, control, Xin, Xout, Yin, Yout, Colour, plot, enable);
-	input clk;
-	input reset_n;
-	input[3:0] control;
-	input[7:0] Xin;
-	input[6:0] Yin;
+	input       clk;
+	input       reset_n;
+	input [3:0] control;
+	input [7:0] Xin;
+	input [6:0] Yin;
 	
-	output[7:0] Xout;
-	output[6:0] Yout;
-	output[2:0] Colour;
-	output plot;
-	output enable;
+	output reg [7:0] Xout;
+	output reg [6:0] Yout;
+	output reg [2:0] Colour;
+	output reg       plot   = 0;
+	output reg       enable = 0;
 	
-	reg[7:0] Xold = 50;
-	reg[6:0] Yold = 50;
-	reg[7:0] Xhold = 50;
-	reg[6:0] Yhold = 50;
-	reg[7:0] Xout = 50;
-	reg[6:0] Yout = 50;
-	reg[2:0] Colour = 3'b100;
-	reg plot = 0;
-	reg enable = 0;
+	reg [7:0] Xhold = 50;
+	reg [6:0] Yhold = 50;
+	reg [1:0] XDraw = 2'b00;
+	reg [1:0] YDraw = 2'b00;
 	
-	reg[1:0] XDraw = 2'b00;
-	reg[1:0] YDraw = 2'b00;
+	localparam S_PREHOLD = 4'b0100,
+				  S_HOLD    = 4'b0000,
+				  S_P_CLEAR = 4'b0001,
+				  S_P_LEFT  = 4'b0011,
+				  S_P_RIGHT = 4'b0010,
+				  S_P_DOWN  = 4'b0110,
+				  S_P_UP    = 4'b0111,
+				  S_P_DRAW  = 4'b0101;
 	
 	always @ (posedge clk, negedge reset_n)
 	begin
 		if(~reset_n)
 		begin
-			Xold <= Xin;
-			Yold <= Yin;
-			Xout <= 50;
-			Yout <= 50;
+			Xhold  <= Xin;
+			Yhold  <= Yin;
+			Xout   <= 50;
+			Yout   <= 50;
+			XDraw  <= 2'b00;
+			YDraw  <= 2'b00;
 			Colour <= 3'b100;
-			XDraw <= 2'b00;
-			YDraw <= 2'b00;
 		end
 		
 		else
 		begin
 			case(control)
-				4'b0001: // Left
+				S_P_CLEAR:
+				begin
+					Colour <= 3'b000;
+				end
+			
+				S_P_LEFT: 
 				begin
 					if(Xin > 0)
 						Xhold <= Xin - 1;
@@ -48,7 +54,7 @@ module MovementDatapath(clk, reset_n, control, Xin, Xout, Yin, Yout, Colour, plo
 						Xhold <= Xin;
 				end
 				
-				4'b0010: // Right
+				S_P_RIGHT: 
 				begin
 					if(Xin < 160) // Max width
 						Xhold <= Xin + 1;
@@ -56,15 +62,7 @@ module MovementDatapath(clk, reset_n, control, Xin, Xout, Yin, Yout, Colour, plo
 						Xhold <= Xin;
 				end
 				
-				4'b0011: // UP
-				begin
-					if(Yin > 0)
-						Yhold <= Yin - 1;
-					else
-						Yhold <= Yin;
-				end
-				
-				4'b0100: // Down
+				S_P_DOWN:
 				begin
 					if(Yin < 120) // Max height
 						Yhold <= Yin + 1;
@@ -72,52 +70,45 @@ module MovementDatapath(clk, reset_n, control, Xin, Xout, Yin, Yout, Colour, plo
 						Yhold <= Yin;
 				end
 				
-				4'b0101: // Clear
+				S_P_UP: // UP
 				begin
-					Colour <= 3'b000;
+					if(Yin > 0)
+						Yhold <= Yin - 1;
+					else
+						Yhold <= Yin;
 				end
 				
-				4'b0110: // Draw
+				S_P_DRAW: // Draw
 				begin
-					if(enable)
-					begin
-						enable <= 0;
-						XDraw <= 2'b00;
-						YDraw <= 2'b00;
-						Xold <= Xhold;
-						Yold <= Yhold;
-					end
-
 					Colour <= 3'b100;
 				end
 			endcase
 			
-			if(control == 4'b0101 || control == 4'b0110)
+			if(control == S_P_CLEAR || control == S_P_DRAW)
 			begin
-				plot <= 1;
+				plot <= 1; // Allows VGA to display the current pixel
 				
-				Xout <= Xold + XDraw;
-				Yout <= Yold + YDraw;
+				Xout <= Xhold + XDraw; // Sets the current x pixel @ current draw + increment
+				Yout <= Yhold + YDraw; // y pixel
 				
 				if(XDraw == 2'b11)
 				begin
 					if(YDraw == 2'b11)
 					begin
-						enable <= 1;
+						enable <= 1; // Tell the FSM that we are done drawing.
+						// (Don't plot <= 0 here because the last pixel still needs to be drawn first)
 					end
 					
-					YDraw <= YDraw + 1;
+					YDraw <= YDraw + 1; // Increase y increment
 				end
 				
-				XDraw <= XDraw + 1;
+				XDraw <= XDraw + 1; // Increase x increment
 			end
 			
 			else
 			begin
 				enable <= 0;
-				XDraw <= 2'b00;
-				YDraw <= 2'b00;
-				plot <= 0;
+				plot   <= 0;
 			end
 		end
 	end
--- MovementFSM.v
+++ MovementFSM.v
@@ -1,22 +1,25 @@
-module MovementFSM(clk, reset_n, KEY, STATE, enableDraw, enable);
+module MovementFSM(clk, reset_n, KEY, STATE, doneDrawing, delayedClk);
 	input clk;
 	input reset_n;
-	input[3:0] KEY;
-	input enableDraw;
-	input enable;
+	input [3:0] KEY;
+	input doneDrawing;
+	input delayedClk;
 	
-	output[3:0] STATE;
-
-	reg[3:0] STATE = 4'b0000;
+	output reg [3:0] STATE = 4'b0000;
 	
-	localparam S_HOLD = 4'b0000,
-				  S_P_LEFT = 4'b0001,
+	wire RIGHT = ~KEY[0];
+	wire DOWN  = ~KEY[1];
+	wire UP    = ~KEY[2];
+	wire LEFT  = ~KEY[3];
+	
+	localparam S_PREHOLD = 4'b0100,
+				  S_HOLD    = 4'b0000,
+				  S_P_CLEAR = 4'b0001,
+				  S_P_LEFT  = 4'b0011,
 				  S_P_RIGHT = 4'b0010,
-				  S_P_UP = 4'b0011,
-				  S_P_DOWN = 4'b0100,
-				  S_P_CLEAR = 4'b0101,
-				  S_P_DRAW = 4'b0110,
-				  S_PREHOLD = 4'b0111;
+				  S_P_DOWN  = 4'b0110,
+				  S_P_UP    = 4'b0111,
+				  S_P_DRAW  = 4'b0101;
 	
 	always @ (posedge clk, negedge reset_n)
 	begin
@@ -28,15 +31,9 @@ module MovementFSM(clk, reset_n, KEY, STATE, enableDraw, enable);
 		else
 		begin
 			case(STATE)
-				/*
-				KEY[0] = RIGHT
-				KEY[1] = DOWN
-				KEY[2] = UP
-				KEY[3] = LEFT
-				*/
 				S_PREHOLD:
 				begin
-					if(~enable)
+					if(~delayedClk) // Until off tick
 						STATE <= S_HOLD;
 					else
 						STATE <= S_PREHOLD;
@@ -44,62 +41,62 @@ module MovementFSM(clk, reset_n, KEY, STATE, enableDraw, enable);
 				
 				S_HOLD:
 				begin
-					if(enable)
-					begin
-						if(~KEY[0])
-							STATE <= S_P_RIGHT;
-						else if(~KEY[3])
-							STATE <= S_P_LEFT;
-						else if(~KEY[1])
-							STATE <= S_P_DOWN;
-						else if(~KEY[2])
-							STATE <= S_P_UP;
-						else
-							STATE <= S_HOLD;
-					end
-					
+					if(delayedClk) // Until on tick
+						STATE <= S_P_CLEAR;
 					else
 						STATE <= S_HOLD;
 				end
 				
-				S_P_RIGHT:
+				S_P_CLEAR: // Wipes away previous failures
 				begin
-					if(~KEY[1])
-						STATE <= S_P_DOWN;
-					else if(~KEY[2])
-						STATE <= S_P_UP;
+					if(doneDrawing)
+						if(RIGHT)
+							STATE <= S_P_RIGHT;
+						else if(LEFT)
+							STATE <= S_P_LEFT;
+						else if(DOWN)
+							STATE <= S_P_DOWN;
+						else if(UP)
+							STATE <= S_P_UP;
+						else
+							STATE <= S_P_DRAW;
 					else
 						STATE <= S_P_CLEAR;
+				end
+				
+				S_P_RIGHT:
+				begin
+					if(DOWN)
+						STATE <= S_P_DOWN;
+					else if(UP)
+						STATE <= S_P_UP;
+					else
+						STATE <= S_P_DRAW;
 				end
 				
 				S_P_LEFT:
 				begin
-					if(~KEY[1])
+					if(DOWN)
 						STATE <= S_P_DOWN;
-					else if(~KEY[2])
+					else if(UP)
 						STATE <= S_P_UP;
 					else
-						STATE <= S_P_CLEAR;
+						STATE <= S_P_DRAW;
 				end
 				
 				S_P_UP:
-					STATE <= S_P_CLEAR;
+					STATE <= S_P_DRAW;
 				
 				S_P_DOWN:
-					STATE <= S_P_CLEAR;
-					
-				S_P_CLEAR:
-				begin
-					if(enableDraw)
-						STATE <= S_P_DRAW;
-					else
-						STATE <= S_P_CLEAR;
-				end
+					STATE <= S_P_DRAW;
 				
 				S_P_DRAW:
 				begin
-					if(enableDraw)
-						STATE <= S_PREHOLD;
+					if(doneDrawing)
+						if(delayedClk)
+							STATE <= S_PREHOLD;
+						else
+							STATE <= S_HOLD;
 					else
 						STATE <= S_P_DRAW;
 				end
