c48d3892ef524b448932a6b4a11c2f25c0796118
AlexReymer <alexreymer@gmail.com>
2018-03-20 15:47:13 -0400

    Added fsm and datapath for the bird, also added code for linear feedback shift register.

---
+++ BirdDatapath.v
@@ -0,0 +1,166 @@
+module BirdDatapath(clk, reset_n, control, Xin, Xout, Yin, Yout, Colour, plot, enable);
+	input clk;
+	input reset_n;
+	input[3:0] control;
+	input[7:0] Xin;
+	input[6:0] Yin;
+	
+	output[7:0] Xout;
+	output[6:0] Yout;
+	output[2:0] Colour;
+	output plot;
+	output enable;
+	output flying;
+	
+	reg[7:0] Xold = 80;
+	reg[6:0] Yold = 90;
+	reg[7:0] Xhold = 80;
+	reg[6:0] Yhold = 90;
+	reg[7:0] Xout = 80;
+	reg[6:0] Yout = 90;
+	reg[2:0] Colour = 3'b111;
+	reg plot = 0;
+	reg enable = 0;
+	reg flying = 0;
+	
+	reg[1:0] XDraw = 2'b00;
+	reg[1:0] YDraw = 2'b00;
+	
+	always @ (posedge clk, negedge reset_n)
+	begin
+		if(~reset_n)
+		begin
+			Xold <= Xin;
+			Yold <= Yin;
+			Xout <= 80;
+			Yout <= 90;
+			Colour <= 3'b111;
+			XDraw <= 2'b00;
+			YDraw <= 2'b00;
+		end
+		
+		else
+		begin
+			case(control)
+				4'b0001: // Left
+				begin
+					if(Xin > 0)
+						Xhold <= Xin - 1;
+					else
+						Xhold <= Xin;
+				end
+				
+				4'b0010: // Right
+				begin
+					if(Xin < 160) // Max width
+						Xhold <= Xin + 1;
+					else
+						Xhold <= Xin;
+				end
+				
+				4'b0011: // UP
+				begin
+					if(Yin > 0)
+						Yhold <= Yin - 1;
+					else
+						Yhold <= Yin;
+				end
+				
+				4'b0100: // Down
+				begin
+					if(Yin < 120) // Max height
+						Yhold <= Yin + 1;
+					else
+						Yhold <= Yin;
+				end
+				
+				4'b0101: // Clear
+				begin
+					Colour <= 3'b000;
+				end
+				
+				4'b0110: // Draw
+				begin
+					if(enable)
+					begin
+						enable <= 0;
+						XDraw <= 2'b00;
+						YDraw <= 2'b00;
+						Xold <= Xhold;
+						Yold <= Yhold;
+					end
+
+					Colour <= 3'b111;
+				end
+				
+				4'b0111: // Shot
+				begin
+					if(enable && Yin < 120)
+					begin
+						Yhold <= Yin + 1;
+						flying <= 1;
+					end
+					else
+						flying <= 0;
+					if(enable && flying)
+					begin
+						enable <= 0;
+						XDraw <= 2'b00;
+						YDraw <= 2'b00;
+						Yold <= Yhold;
+					end
+
+					Colour <= 3'b111;
+				end
+				
+				4'b1000: // Escape
+				begin
+					if(enable && Yin > 0)
+					begin
+						Yhold <= Yin - 1;
+						flying <= 1;
+					end
+					else
+						flying <= 0;
+					if(enable && flying)
+					begin
+						enable <= 0;
+						XDraw <= 2'b00;
+						YDraw <= 2'b00;
+						Yold <= Yhold;
+					end
+
+					Colour <= 3'b111;
+				end
+			endcase
+			
+			if(control == 4'b0101 || control == 4'b0110)
+			begin
+				plot <= 1;
+				
+				Xout <= Xold + XDraw;
+				Yout <= Yold + YDraw;
+				
+				if(XDraw == 2'b11)
+				begin
+					if(YDraw == 2'b11)
+					begin
+						enable <= 1;
+					end
+					
+					YDraw <= YDraw + 1;
+				end
+				
+				XDraw <= XDraw + 1;
+			end
+			
+			else
+			begin
+				enable <= 0;
+				XDraw <= 2'b00;
+				YDraw <= 2'b00;
+				plot <= 0;
+			end
+		end
+	end
+endmodule
---
+++ BirdFSM.v
@@ -0,0 +1,135 @@
+module BirdFSM(clk, reset_n, enable, STATE, enableDraw, shot, outOfAmmo, flying);
+	input clk;
+	input reset_n;
+	input enable;
+	input enableDraw;
+	input shot;
+	input outOfTime;
+	
+	wire [7:0] rand;
+	wire [3:0] move;
+	wire overflow;
+	
+	assign move = rand[3:0];
+	
+	lfsr_updown L0(clk, ~reset_n, enable, 1'b1, rand, overflow);  
+	
+	output[3:0] STATE;
+
+	reg[3:0] STATE = 4'b1001;
+	
+	localparam S_HOLD = 4'b0000,
+				  S_B_LEFT = 4'b0001,
+				  S_B_RIGHT = 4'b0010,
+				  S_B_UP = 4'b0011,
+				  S_B_DOWN = 4'b0100,
+				  S_B_CLEAR = 4'b0101,
+				  S_B_DRAW = 4'b0110,
+				  S_B_SHOT = 4'b0111,
+				  S_B_ESCAPE = 4'b1000,
+				  S_PREHOLD = 4'b1001;
+				  
+	
+	always @ (posedge clk, negedge reset_n)
+	begin
+		if(~reset_n)
+		begin
+			STATE <= 4'b0000;
+		end
+		
+		else
+		begin
+			case(STATE)
+				S_PREHOLD:
+				begin
+					if(~enable)
+						STATE <= S_HOLD;
+					else
+						STATE <= S_PREHOLD;
+				end
+				
+				S_B_SHOT:
+				begin
+					if(enableDraw &&~flying)
+						STATE <= S_PREHOLD;
+					else
+						STATE <= S_B_SHOT;
+				end
+				
+				S_B_ESCAPE:
+				begin
+					if(enableDraw &&~flying)
+						STATE <= S_PREHOLD;
+					else
+						STATE <= S_B_ESCAPE;
+				end
+				
+				S_HOLD:
+				begin
+					if(enable)
+					begin
+						if(move[0])
+							STATE <= S_B_RIGHT;
+						else if(move[1])
+							STATE <= S_B_LEFT;
+						else if(move[2])
+							STATE <= S_B_DOWN;
+						else if(move[3])
+							STATE <= S_B_UP;
+						else
+							STATE <= S_HOLD;
+					end
+					
+					else
+						STATE <= S_HOLD;
+				end
+				
+				S_B_RIGHT:
+				begin
+					if(move[2])
+						STATE <= S_B_DOWN;
+					else if(move[3])
+						STATE <= S_B_UP;
+					else
+						STATE <= S_B_CLEAR;
+				end
+				
+				S_B_LEFT:
+				begin
+					if(move[2])
+						STATE <= S_B_DOWN;
+					else if(move[3])
+						STATE <= S_B_UP;
+					else
+						STATE <= S_B_CLEAR;
+				end
+				
+				S_B_UP:
+					STATE <= S_B_CLEAR;
+				
+				S_B_DOWN:
+					STATE <= S_B_CLEAR;
+					
+				S_B_CLEAR:
+				begin
+					if(enableDraw)
+						STATE <= S_B_DRAW;
+					else
+						STATE <= S_B_CLEAR;
+				end
+				
+				S_B_DRAW:
+				begin
+					if(enableDraw && shot)
+						STATE <= S_B_SHOT;
+					else if(enableDraw && outOfAmmo)
+						state <= S_B_ESCAPE;
+					else if(enableDraw)
+						STATE <= S_HOLD
+					else
+						STATE <= S_B_DRAW;
+				end
+			endcase
+		end
+	end
+endmodule
